df4fbff3310c931e50100cb86a993476a2e87f4b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-02-18 14:26:37 -0300

    Boot cache

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,14 +3,14 @@
 namespace PragmaRX\CountriesLaravel\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use PragmaRX\CountriesLaravel\Package\ServiceProvider as CountriesServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
     protected function getPackageProviders($app)
     {
         return [
-            CountriesServiceProvider::class,
+            \Illuminate\Cache\CacheServiceProvider::class,
+            \PragmaRX\CountriesLaravel\Package\ServiceProvider::class,
         ];
     }
 }
